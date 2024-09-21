2a31922b28a2a9aa7e842c8157f8874978f71e47
hoanbklucky <ngothanhhoan@gmail.com>
2024-09-21 08:42:25 -0400

    change test code hw3_1_test, i.e. remove complex error message by a simple error message, to work with autograding

--- hw3_0_test.py
+++ hw3_0_test.py
@@ -3,7 +3,7 @@ import hw3_0
 def test_print_numbers(capsys):
     hw3_0.print_numbers()
     captured = capsys.readouterr()
-    assert captured.out == "0\n1\n2\n3\n4\n", "Failed"
+    assert captured.out == "0\n1\n2\n3\n4\n", "Failed to print numbers from 0 to 4, each on a new line"
     print("passed")
 
 # To run this test, you need to type into the terminal: pytest hw3_0_test.py
--- hw3_1_test.py
+++ hw3_1_test.py
@@ -3,6 +3,6 @@ import hw3_1
 def test_print_hello(capsys):
     hw3_1.print_hello()
     captured = capsys.readouterr()
-    assert captured.out == "Hello\n"*10, f"Expected output was {'Hello\n'*10} but got {captured.out}"
+    assert captured.out == "Hello\n"*10, "Failed to print 10 lines of Hello"
     
 # To run this test, you need to type into the terminal: pytest hw3_1_test.py
